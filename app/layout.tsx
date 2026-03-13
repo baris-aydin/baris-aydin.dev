0734d4c87604e3059e02f1749e424a17f723a5ea
baris-aydin <brsalpaydn@gmail.com>
2026-03-13 00:02:49 -0400

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,23 @@
 import "./globals.css";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-16MNFX4WB5"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-16MNFX4WB5');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
